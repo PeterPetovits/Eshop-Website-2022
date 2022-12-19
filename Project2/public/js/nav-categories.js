c02393d5e9080059f12f39381223b123d444f82c
Peter <peterp@outlook.com.gr>
2022-12-20 00:42:25 +0200

    project 2 structure and init case-one

---
+++ Project2/public/js/nav-categories.js
@@ -0,0 +1,30 @@
+window.onload = function(){
+    let categoriesTemplate = document.getElementById("product-categories-template").innerHTML;
+
+    console.log(categoriesTemplate)
+
+    let compiledTemplate = Handlebars.compile(categoriesTemplate);
+
+    console.log(compiledTemplate)
+
+    let myHeaders = new Headers();
+    myHeaders.append('Accept', 'application/json');
+
+    let init = {
+        method: "GET",
+        headers: myHeaders
+    }
+
+    fetch("https://wiki-shop.onrender.com/categories", init)
+    .then(function(response){
+       return response.json();
+    })
+    .then(function(obj){
+        let content = compiledTemplate(obj);
+        let div = document.getElementById("multi-items");
+        div.innerHTML = content;
+    })
+    .catch(error =>{
+        console.log(error);
+    })
+}
