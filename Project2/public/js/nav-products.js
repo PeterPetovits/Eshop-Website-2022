497d50e4ebb7348001655fd72f07d483d08e1b51
Alkis Pouros <alkispou35@gmail.com>
2022-12-20 12:52:40 +0200

    added products fetching

---
+++ Project2/public/js/nav-products.js
@@ -0,0 +1,67 @@
+window.onload=function(){
+    let products_template = document.getElementById("products-template").innerHTML;
+    console.log(products_template);
+
+
+    let compiledTemplate = Handlebars.compile(products_template);
+    console.log(compiledTemplate)
+
+    let myHeaders = new Headers();
+    myHeaders.append('Accept', 'application/json');
+
+    let init = {
+        method: "GET",
+        headers: myHeaders
+    }
+    let url_str = "https://wiki-shop.onrender.com/categories/?id"
+    let url = new URL(url_str);
+    let search_params = url.searchParams;
+
+
+    if(search_params.get('id')==0){
+         fetch("https://wiki-shop.onrender.com/categories/:0/products",init)
+         .then(function(response){
+            return response.json();
+         })
+         .then(function(obj){
+            let content = compiledTemplate(obj);
+            console.log(obj);
+            let div = document.getElementById("multi-items");
+            div.innerHTML = content;
+         })
+         .catch(error=>{
+            console.log("an error occured",error);
+         })
+      }
+
+    if(search_params.get('id')==1){
+         fetch("https://wiki-shop.onrender.com/categories/:1/products",init)
+         .then(function(response){
+            return response.json();
+         })
+         .then(function(obj){
+            let content = compiledTemplate(obj);
+            console.log(obj);
+            let div = document.getElementById("multi-items");
+            div.innerHTML = content;
+         })
+         .catch(error=>{
+            console.log("an error occured",error);
+         })
+    }
+    if(search_params.get('id')==2){
+         fetch("https://wiki-shop.onrender.com/categories/:2/products",init)
+         .then(function(response){
+            return response.json();
+         })
+         .then(function(obj){
+            let content = compiledTemplate(obj);
+            console.log(obj);
+            let div = document.getElementById("multi-items");
+            div.innerHTML = content;
+         })
+         .catch(error=>{
+            console.log("an error occured",error);
+         })
+      }
+}
